9327025f10683125d8f19029946a9a2499667bda
Batcha <batcha.ashik15@gmail.com>
2021-05-03 05:06:56 +0000

    Removing old stuffs

--- Django/personal_portfolio-project/personal_portfolio/settings.py
+++ Django/personal_portfolio-project/personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7pw_7+909m4w42^lr2hu&lb(l*dcs=hvvg5yd!2&u-)h$eo26m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['batchaashik.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
